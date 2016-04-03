7944fde13f602421a05b9fc7f77c68e217349434
seba1601 <seba1601@gmail.com>
2016-04-03 19:30:38 -0300

    Version 1
    Programa completo

---
+++ BLL/LivianoBLL.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Cotizacion.BLL
+{
+    
+    class LivianoBLL
+    {
+        EntidadBDCotizacion contex = new EntidadBDCotizacion();
+
+        public string ObtenerPermiso(string Codigo, int Anno) 
+        {
+            Livianos busqueda = (from l in contex.Livianos where l.Anno==Anno && l.Codigo==Codigo select l).FirstOrDefault();
+            if(busqueda.Anno>0){                  
+                return string.Format("Tipo de vehiculo: {0} \nMarca: {1} \nModelo: {2} \nCilindrada: {3} \nCombustible: {4} \nTransmisión: {5} \nTasación: ${6} \nPermiso: ${7}\n", busqueda.Tipo, busqueda.Marca, busqueda.Modelo, busqueda.Cilindros, busqueda.Combustible, busqueda.Transmision, busqueda.Tasacion, busqueda.Permiso);      
+            }else{
+                return string.Format("El codigo y el año no pertenecen a ningun vehiculo");
+            }
+            
+        }
+
+        // Tablas
+
+        public List<Livianos> ObtenerTodos() {
+            return (from l in contex.Livianos select l).ToList();
+        }
+
+        public List<Livianos> ObtenerPorTipo(string tipo) {
+            return (from l in contex.Livianos where l.Tipo == tipo select l).ToList();
+        }
+
+        public List<Livianos> ObtenerPorAnno(string tipo, int anno, string marca)
+        {
+            return (from l in contex.Livianos where l.Tipo == tipo && l.Anno == anno && l.Marca == marca select l).Distinct().ToList();
+        }
+
+        public List<Livianos> ObtenerPorMarca(string tipo, string marca){
+            return (from l in contex.Livianos where l.Tipo == tipo && l.Marca == marca select l).Distinct().ToList();
+        }
+
+        public List<Livianos> ObtenerPorCilindrada(string tipo, int anno, string marca, string cilindrada) {
+            return (from l in contex.Livianos where l.Tipo == tipo && l.Anno == anno && l.Marca == marca && l.Cilindros==cilindrada select l).Distinct().ToList();
+        }
+
+        public List<Livianos> ObtenerPorPuertas(string tipo, int anno, string marca, string cilindrada, string puertas)
+        {
+            return (from l in contex.Livianos where l.Tipo == tipo && l.Anno == anno && l.Marca == marca && l.Cilindros == cilindrada && l.Puertas==puertas select l).Distinct().ToList();
+        }
+
+        public List<Livianos> ObtenerPorTransmision(string tipo, int anno, string marca, string cilindrada, string puertas, string transmision)
+        {
+            return (from l in contex.Livianos where l.Tipo == tipo && l.Anno == anno && l.Marca == marca && l.Cilindros == cilindrada && l.Puertas == puertas && l.Transmision == transmision select l).Distinct().ToList();
+        }
+
+        // Listados
+
+        public List<String> ListadoTipos() {
+            return (from l in contex.Livianos select l.Tipo).Distinct().ToList();
+        }
+
+        public List<String> ListadoMarcas(string tipo) {
+            return (from l in contex.Livianos where l.Tipo == tipo select l.Marca).Distinct().ToList();
+        }
+
+        public List<String> ListadoAnnos(string tipo, string marca){
+            var listado = (from l in contex.Livianos where l.Tipo == tipo && l.Marca == marca select l.Anno).Distinct().ToList();
+            List<String> list = new List<string>();
+            foreach (var c in listado)
+            {
+                list.Add(c.ToString());
+            }
+            return list;
+        }
+
+        public List<String> ListadoCilindros(string tipo, string marca, int anno) {
+            return (from l in contex.Livianos where l.Tipo == tipo && l.Marca == marca && l.Anno == anno select l.Cilindros).Distinct().ToList();
+        }
+
+        public List<String> ListadoPuertas(string tipo, string marca, int anno, string cilindrada)
+        {
+            return (from l in contex.Livianos where l.Tipo == tipo && l.Marca == marca && l.Anno == anno && l.Cilindros==cilindrada select l.Puertas).Distinct().ToList();
+        }
+
+        public List<String> ListadoTransmision(string tipo, string marca, int anno, string cilindrada, string puertas)
+        {
+            return (from l in contex.Livianos where l.Tipo == tipo && l.Marca == marca && l.Anno == anno && l.Cilindros == cilindrada && l.Puertas == puertas select l.Transmision).Distinct().ToList();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Cotizacion.BLL;
+
+namespace Cotizacion
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LivianoBLL lbll = new LivianoBLL();
+            CmbTipo.ItemsSource = lbll.ListadoTipos();
+            DgTabla.ItemsSource = lbll.ObtenerTodos();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LivianoBLL lbll = new LivianoBLL();
+            MessageBox.Show(lbll.ObtenerPermiso(TxtCodigo.Text.Trim().ToUpper(), Convert.ToInt32(TxtAnno.Text.Trim())));                     
+        }
+
+        private void CmbTipo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LivianoBLL lbll = new LivianoBLL();
+            CmbMarcas.ItemsSource = null;
+            CmbAnno.ItemsSource = null;
+            try
+            {
+                CmbMarcas.ItemsSource = lbll.ListadoMarcas(CmbTipo.SelectedValue.ToString());
+                DgTabla.ItemsSource = lbll.ObtenerPorTipo(CmbTipo.SelectedValue.ToString());
+            }
+            catch 
+            {
+            }
+        }
+
+        private void CmbMarcas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LivianoBLL lbll = new LivianoBLL();
+            CmbAnno.ItemsSource = null;
+            try
+            {
+                CmbAnno.ItemsSource = null;
+                CmbAnno.ItemsSource = lbll.ListadoAnnos(CmbTipo.SelectedValue.ToString(), CmbMarcas.SelectedValue.ToString());
+                DgTabla.ItemsSource = lbll.ObtenerPorMarca(CmbTipo.SelectedValue.ToString(), CmbMarcas.SelectedValue.ToString());
+            }
+            catch (Exception)
+            {
+                CmbAnno.ItemsSource = null;
+            }
+            
+        }
+
+        private void CmbAnno_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LivianoBLL lbll = new LivianoBLL();
+            try
+            {
+                CmbCilindros.ItemsSource = null;
+                CmbCilindros.ItemsSource = lbll.ListadoCilindros(CmbTipo.SelectedValue.ToString(), CmbMarcas.SelectedValue.ToString(), Convert.ToInt32(CmbAnno.SelectedValue.ToString()));
+                DgTabla.ItemsSource = lbll.ObtenerPorAnno(CmbTipo.SelectedValue.ToString(), Convert.ToInt32(CmbAnno.SelectedValue.ToString()), CmbMarcas.SelectedValue.ToString());
+            }
+            catch (Exception)
+            {
+                CmbCilindros.ItemsSource = null;
+            }
+        }
+
+        private void CmbCilindros_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LivianoBLL lbll = new LivianoBLL();
+            try
+            {
+               CmbPuertas.ItemsSource = null;
+               CmbPuertas.ItemsSource = lbll.ListadoPuertas(CmbTipo.SelectedValue.ToString(), CmbMarcas.SelectedValue.ToString(), Convert.ToInt32(CmbAnno.SelectedValue.ToString()), CmbCilindros.SelectedValue.ToString());
+               DgTabla.ItemsSource = lbll.ObtenerPorCilindrada(CmbTipo.SelectedValue.ToString(), Convert.ToInt32(CmbAnno.SelectedValue.ToString()), CmbMarcas.SelectedValue.ToString(), CmbCilindros.SelectedValue.ToString());
+            }
+            catch (Exception)
+            {
+                CmbPuertas.ItemsSource = null;
+            }
+        }
+
+        private void CmbPuertas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LivianoBLL lbll = new LivianoBLL();
+            try
+            {
+                CmbTransmision.ItemsSource = null;
+                CmbTransmision.ItemsSource = lbll.ListadoTransmision(CmbTipo.SelectedValue.ToString(), CmbMarcas.SelectedValue.ToString(), Convert.ToInt32(CmbAnno.SelectedValue.ToString()), CmbCilindros.SelectedValue.ToString(), CmbPuertas.SelectedValue.ToString());
+                DgTabla.ItemsSource = lbll.ObtenerPorPuertas(CmbTipo.SelectedValue.ToString(), Convert.ToInt32(CmbAnno.SelectedValue.ToString()), CmbMarcas.SelectedValue.ToString(), CmbCilindros.SelectedValue.ToString(), CmbPuertas.SelectedValue.ToString());
+            }
+            catch (Exception)
+            {
+                CmbTransmision.ItemsSource = null;
+            }
+        }
+
+        private void CmbTransmision_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LivianoBLL lbll = new LivianoBLL();
+            try
+            {
+                DgTabla.ItemsSource = lbll.ObtenerPorTransmision(CmbTipo.SelectedValue.ToString(), Convert.ToInt32(CmbAnno.SelectedValue.ToString()), CmbMarcas.SelectedValue.ToString(), CmbCilindros.SelectedValue.ToString(), CmbPuertas.SelectedValue.ToString(), CmbTransmision.SelectedValue.ToString());
+            }
+            catch (Exception)
+            {
+                CmbTransmision.ItemsSource = null;
+            }
+        }
+
+        private void BtnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            LivianoBLL lbll = new LivianoBLL();
+            CmbTransmision.ItemsSource = null;
+            CmbPuertas.ItemsSource = null;
+            CmbCilindros.ItemsSource = null;
+            CmbAnno.ItemsSource = null;
+            CmbMarcas.ItemsSource = null;
+            CmbTipo.ItemsSource = null;
+            CmbTipo.ItemsSource = lbll.ListadoTipos();
+            DgTabla.ItemsSource = lbll.ObtenerTodos();
+            TxtAnno.Text = "";
+            TxtCodigo.Text = "";
+                        
+        }
+    }
+}
